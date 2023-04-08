3dbd9a9eea9ffc6782d9ea7fe9fac782088c5140
alprostov <alprostov.1982@gmail.com>
2023-04-08 11:57:29 +0400

    test23

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,10 +1,10 @@
 class User:
-    def __init__(self, name, email):
-        self.name = name
+    def __init__(self, name1, email):
+        self.name2 = name1
         self.email = email
 
 peter = User(name="Peter Robertson", email="peterrobertson@mail.com")
 julia = User(name="Julia Donaldson", email="juliadonaldson@mail.com")
 
-print(peter.name)
+print(peter.name2)
 print(julia.email)
