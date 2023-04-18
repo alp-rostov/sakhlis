9bc36cc6b2f2974ac0cdb58a2ca05aec4cdaf749
alprostov <alprostov.1982@gmail.com>
2023-04-19 00:54:17 +0400

    project Sakhlis -> add update/delete views for model repaierlist

--- sakhlis/web/app_site/urls.py
+++ sakhlis/web/app_site/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RepairerL, RepaierD, Repaier_create
+from .views import RepairerL, RepaierD, RepaierCreate, RepaierUpdate, RepaierDelete
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,8 +7,9 @@ from django.conf.urls.static import static
 urlpatterns = [
 path('', RepairerL.as_view()),
 path('<int:pk>', RepaierD.as_view(), name='datail_repair'),
-path('create', Repaier_create.as_view()),
-
+path('create', RepaierCreate.as_view()),
+path('create/<int:pk>', RepaierUpdate.as_view()),
+path('delete/<int:pk>', RepaierDelete.as_view()),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # для выгрузки картинок из БД в шаблон.
--- sakhlis/web/app_site/views.py
+++ sakhlis/web/app_site/views.py
@@ -1,19 +1,20 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import RepairerList
 from .filters import RepFilter
 from .forms import RepairerForm
 
+
 # Create your views here.
-class RepairerL( ListView):
+class RepairerL(ListView):
     model = RepairerList
     context_object_name = 'repairer'
     template_name = 'repairerlist.html'
     paginate_by = 2
+
     def get_queryset(self):
         queryset = super().get_queryset()
         self.filterset = RepFilter(self.request.GET, queryset)
         return self.filterset.qs
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -27,11 +28,19 @@ class RepaierD(DetailView):
     template_name = 'repaierdar.html'
     context_object_name = 'rep'
 
-class Repaier_create(CreateView):
+
+class RepaierCreate(CreateView):
     model = RepairerList
     template_name = 'repaier_create.html'
     form_class = RepairerForm
 
 
-    # Переопределяем функцию получения списка
+class RepaierUpdate(UpdateView):
+    model = RepairerList
+    template_name = 'repaier_create.html'
+    form_class = RepairerForm
 
+class RepaierDelete(DeleteView):
+    model = RepairerList
+    template_name = 'repaier_delete.html'
+    success_url = '/'
