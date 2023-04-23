5288c60f260773efdbca49720c87e41526208d91
alprostov <alprostov.1982@gmail.com>
2023-04-23 20:35:28 +0400

    downgraded the version django 4 -> 3.2 and add models

--- DjangiCMS/tutorial/mysite/mysite/settings.py
+++ DjangiCMS/tutorial/mysite/mysite/settings.py
@@ -181,6 +181,7 @@ INSTALLED_APPS = [
     'djangocms_googlemap',
     'djangocms_video',
     'mysite',
+    'www',
 ]
 
 LANGUAGES = (
--- DjangiCMS/tutorial/mysite/www/admin.py
+++ DjangiCMS/tutorial/mysite/www/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CityDirectory, RepairerList, OrderList
 
+admin.site.register(CityDirectory)
+admin.site.register(RepairerList)
+admin.site.register(OrderList)
 # Register your models here.
