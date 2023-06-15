9104f093fc791b0619ad9c83616483e16d6f4bd8
alprostov <alp-rostov@mail.ru>
2023-06-15 21:32:46 +0400

    add button 'add-more' in template 'invoice.html'

--- sakhlis/web/app_site/tests.py
+++ sakhlis/web/app_site/tests.py
@@ -1,21 +1,9 @@
-# from utils import *
-#
-#
-# assert set_coordinates_address('Руставели', 'Тбилиси'), 'Тест не пройден1'
-# assert set_coordinates_address('', 'Тбилиси'), 'Тест не пройден2'
-# assert set_coordinates_address(222, 222), 'Тест не пройден5'
-#
-# assert set_coordinates_address(['Руставели'], 'Тбилиси'), 'Тест не пройден3'
-# assert set_coordinates_address('Руставели', {'Тбилиси'}), 'Тест не пройден4'
-import itertools
-from pprint import pprint
+from utils import *
 
 
+assert set_coordinates_address('Руставели', 'Тбилиси'), 'Тест не пройден1'
+assert set_coordinates_address('', 'Тбилиси'), 'Тест не пройден2'
+assert set_coordinates_address(222, 222), 'Тест не пройден5'
 
-def grouper(iterable, n):
-    args = [iter(iterable)]*n
-    print(*args)
-    return itertools.zip_longest(*args)
-
-print(next(grouper((1,'cndf',3), 5)))
-print(next(grouper((1,'cndf',3), 4)))
+assert set_coordinates_address(['Руставели'], 'Тбилиси'), 'Тест не пройден3'
+assert set_coordinates_address('Руставели', {'Тбилиси'}), 'Тест не пройден4'
