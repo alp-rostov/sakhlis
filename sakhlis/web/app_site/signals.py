de4b043f476c204bc2376a1fca3964ee3221ecf8
alprostov1982_home <alp-rostov@mail.ru>
2023-09-27 23:29:16 +0400

    ------

--- sakhlis/web/app_site/signals.py
+++ sakhlis/web/app_site/signals.py
@@ -11,20 +11,20 @@ from .tasks import send_order_information, send_email_after_registration
 TOKEN = "6082335579:AAHqLPJB2RSdczDSbshpYV5Q7oqmyIcnbFI"
 CHAT_ID = 5621399532
 
-
-@receiver(post_save, sender=OrderList)
-def send_post_new_order(instance, created, **kwargs):
-    """"sent order`s information to telegram group"""
-    try:
-        if created:
-            send_order_information.apply_async([instance.pk], countdown=5, expires=20)
-    except OperationalError:
-        print("ошибка отправки ассинхрон")  #TODO сделать запись в лог файл о несрабатывании ассинхрона
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    """"create empty row in Repaierlist and sent a letter about registration."""
-    if created:
-        RepairerList.objects.create(user=instance)
-        send_email_after_registration.apply_async([instance.pk], countdown=5, expires=20)
-
+#
+# @receiver(post_save, sender=OrderList)
+# def send_post_new_order(instance, created, **kwargs):
+#     """"sent order`s information to telegram group"""
+#     try:
+#         if created:
+#             send_order_information.apply_async([instance.pk], countdown=5, expires=20)
+#     except OperationalError:
+#         print("ошибка отправки ассинхрон")  #TODO сделать запись в лог файл о несрабатывании ассинхрона
+#
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     """"create empty row in Repaierlist and sent a letter about registration."""
+#     if created:
+#         RepairerList.objects.create(user=instance)
+#         send_email_after_registration.apply_async([instance.pk], countdown=5, expires=20)
+#
