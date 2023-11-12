3f9aad401655f4a420d7ab2e67c541abe15dcaa3
alprostov <alp-rostov@mail.ru>
2023-11-12 20:46:16 +0400

    correction template order_list

--- sakhlis/web/app_site/models.py
+++ sakhlis/web/app_site/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.contrib.gis.db import models as geomodel
 from django.core.validators import RegexValidator
 from django.urls import reverse
 
