f806a28ebfa7726a17c8786a4af6550c157d8196
alprostov <alp-rostov@mail.ru>
2024-03-06 09:01:50 +0400

    refactor code - transfer files

--- sakhlis/web/EmailList/website/app_site/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class MyForm(forms.Form):
-    news_text = forms.CharField(label="",
-                             widget=forms.Textarea(
-                                 attrs={"class": "form-control", 'placeholder': "Message text"}
-                             ),
-                                )
-
--- sakhlis/web/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from api.views import StreetView
-
-
-urlpatterns = [
-    path('street/', StreetView.as_view(), name='street'),
-
-]
