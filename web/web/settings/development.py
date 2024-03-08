b95e37055ab0a3424a7fe22bfa91ae0a1a81b42c
alprostov <alp-rostov@mail.ru>
2024-03-08 13:59:19 +0400

    add postgress

--- web/web/settings/development.py
+++ web/web/settings/development.py
@@ -8,22 +8,22 @@ ALLOWED_HOSTS = []
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'sakhlis',
-#         'USER': 'postgres',
-#         'PASSWORD': '1rostov1',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sakhlis',
+        'USER': 'postgres',
+        'PASSWORD': '1rostov1',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
